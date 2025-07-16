084d32a2ab798fbe1090461fcffa088a4c65bd96
Vasileios Fasoulas <vas@MacBookPro.communityfibre.co.uk>
2025-07-16 14:43:43 +0100

    Updated .gitignore
    Excluded Android and iOS dirs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.daa3be81c53f423bb91742f93b433b62',
   appName: 'QR Code Generator',
   webDir: 'dist',
-  server: {
-    url: 'https://daa3be81-c53f-423b-b917-42f93b433b62.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  // server: {
+  //   url: 'https://daa3be81-c53f-423b-b917-42f93b433b62.lovableproject.com?forceHideBadge=true',
+  //   cleartext: true
+  // },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
